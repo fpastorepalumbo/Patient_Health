3e2e9c9a4e4349b5a583ce1fb38ba22658471481
FRANCESCO PIO PALUMBO <pastorefrancescopio@gmail.com>
2024-02-26 18:05:30 +0100

    intreface

--- src/main/java/unisa/diem/patient_health/HelloApplication.java
+++ src/main/java/unisa/diem/patient_health/HelloApplication.java
@@ -12,13 +12,13 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
+        stage.setTitle("Patient Health");
         stage.setScene(scene);
+        stage.setResizable(false);
         stage.show();
-
-        DatasetService datasetUtility = new DatasetService();
-        datasetUtility.loadDataset();
+        //DatasetService datasetUtility = new DatasetService();
+       // datasetUtility.loadDataset();
     }
 
     public static void main(String[] args) {
